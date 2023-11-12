802b17cf18f7d27e63ee5a51f81e908ad8574efd
tobixoxo <sanjeev.singh.rawat28@gmail.com>
2023-11-13 02:02:49 +0530

    setup mongodb

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, jsonify, request, redirect
+from pymongo import MongoClient
+import hashlib
 import json
 
 app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+
+db = client.flask_db
+urls = db.urls
+
 
 if __name__ == "__main__": 
     app.run(debug=True)
@@ -13,8 +20,6 @@ def shortenUrl(longUrl):
 def home():
     return "hello World"
 
-db = {}
-
 @app.route('/shorten/')
 def shorten():
     # take the url
@@ -22,16 +27,18 @@ def shorten():
     # pass it through some shortening service
     shortUrl = shortenUrl(longUrl)
     # save the new url against long url in db
-    db[shortUrl] = longUrl
+    urls.insert_one({shortUrl: longUrl })
     # return response from new url
     return json.dumps({"shortUrl":f"{shortUrl}"})
 
 
 @app.route('/<shortUrl>')
 def extractUrl(shortUrl):
-    #query the long url from database
-    if(shortUrl in db.keys()):
-        longUrl = db[shortUrl]
+    #query the long url db from database
+    record = urls.find_one({shortUrl: {'$exists': True}}) 
+    if(record is not None):
+        print(record)
+        longUrl = record[shortUrl]
         return redirect("https://" + longUrl)
     else:
         return "invalid url"
